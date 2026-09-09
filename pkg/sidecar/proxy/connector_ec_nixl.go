llm-d/llm-d-router

fix: add dedicated APITypeMessages for Anthropic Messages API in routing sidecar
Signed-off-by: greg pereira <grpereir@redhat.com>

pkg/sidecar/proxy/connector_ec_nixl.go
@@ -83,7 +83,7 @@ func (s *Server) fanoutEncoderCollect(
 // handleECNIXL fans out per-image encoder requests, aggregates each
 // response's ec_transfer_params into the prefill request body, and hands
 // off to the configured P/D connector.
-func (s *Server) handleECNIXL(w http.ResponseWriter, r *http.Request, prefillEndPoint string, encodeEndPoints []string) {
+func (s *Server) handleECNIXL(w http.ResponseWriter, r *http.Request, prefillEndPoint string, encodeEndPoints []string, apiType APIType) {
 	s.logger.V(logging.DEBUG).Info("running EC-NIXL protocol", "prefiller", prefillEndPoint, "encoderCount", len(encodeEndPoints))
 
 	_, completionRequest, ok := s.readJSONBody(r, w)
@@ -126,5 +126,5 @@ func (s *Server) handleECNIXL(w http.ResponseWriter, r *http.Request, prefillEnd
 		}
 	}
 
-	s.runPDPipeline(w, r, completionRequest, prefillEndPoint, requestID)
+	s.runPDPipeline(w, r, completionRequest, prefillEndPoint, requestID, apiType)
 }

pkg/sidecar/proxy/connector_ec_shared_storage.go
@@ -38,7 +38,7 @@ func (s *Server) fanoutEncoderPrimer(ctx context.Context, originalRequest map[st
 }
 
 // handleECSharedStorage handles an Encoder-Prefiller-Decoder disaggregation request
-func (s *Server) handleECSharedStorage(w http.ResponseWriter, r *http.Request, prefillEndPoint string, encodeEndPoints []string) {
+func (s *Server) handleECSharedStorage(w http.ResponseWriter, r *http.Request, prefillEndPoint string, encodeEndPoints []string, apiType APIType) {
 	s.logger.V(logging.DEBUG).Info("running EPD protocol", "prefiller", prefillEndPoint, "encoderCount", len(encodeEndPoints))
 
 	_, completionRequest, ok := s.readJSONBody(r, w)
@@ -67,5 +67,5 @@ func (s *Server) handleECSharedStorage(w http.ResponseWriter, r *http.Request, p
 		}
 	}
 
-	s.runPDPipeline(w, r, completionRequest, prefillEndPoint, requestID)
+	s.runPDPipeline(w, r, completionRequest, prefillEndPoint, requestID, apiType)
 }

pkg/sidecar/proxy/connector_nixlv2.go
@@ -75,7 +75,7 @@ func (s *Server) handleNIXLV2(w http.ResponseWriter, r *http.Request, prefillPod
 	if s.config.MoRIIOParallelDispatch && s.config.MoRIIOWriteMode {
 		// MoRI-IO requires transfer_id to carry the "tx" prefix for message routing.
 		transferID := "tx" + uuidStr
-		s.runNIXLProtocolV2WriteParallel(w, r, original, completionRequest, uuidStr, transferID, prefillPodHostPort, kvCacheSource)
+		s.runNIXLProtocolV2WriteParallel(w, r, original, completionRequest, uuidStr, transferID, prefillPodHostPort, kvCacheSource, apiType)
 		return
 	}
 
@@ -444,6 +444,7 @@ retryLoop:
 func (s *Server) runNIXLProtocolV2WriteParallel(
 	w http.ResponseWriter, r *http.Request, original []byte,
 	completionRequest map[string]any, uuidStr, transferID, prefillPodHostPort, kvCacheSource string,
+	apiType APIType,
 ) {
 	s.logger.V(4).Info("running NIXL protocol V2 (concurrent dispatch)",
 		"url", prefillPodHostPort, "request_id", uuidStr)
@@ -456,10 +457,21 @@ func (s *Server) runNIXLProtocolV2WriteParallel(
 	// body; they are restored when building the decode body.
 	streamValue, streamOk := completionRequest[requestFieldStream]
 	streamOptionsValue, streamOptionsOk := completionRequest[requestFieldStreamOptions]
-	maxTokensValue, maxTokensOk := completionRequest[requestFieldMaxTokens]
-	maxCompletionTokensValue, maxCompletionTokensOk := completionRequest[requestFieldMaxCompletionTokens]
-	maxOutputTokensValue, maxOutputTokensOk := completionRequest[requestFieldMaxOutputTokens]
-	minTokensValue, minTokensOk := completionRequest[requestFieldMinTokens]
+
+	type savedField struct {
+		field   string
+		val     any
+		present bool
+	}
+	tokenLimitFields := tokenLimitFieldsForAPIType(apiType)
+	savedTokenValues := make([]savedField, len(tokenLimitFields))
+	for i, field := range tokenLimitFields {
+		if v, ok := completionRequest[field]; ok {
+			savedTokenValues[i] = savedField{field: field, val: v, present: true}
+		} else {
+			savedTokenValues[i] = savedField{field: field}
+		}
+	}
 
 	// Pin both legs to the same DP rank (kv_transfer_params + HTTP header).
 	dpRank := pickDPRank(uuidStr, s.config.MoRIIODPSize)
@@ -501,10 +513,9 @@ func (s *Server) runNIXLProtocolV2WriteParallel(
 
 	completionRequest[requestFieldStream] = false
 	delete(completionRequest, requestFieldStreamOptions)
-	completionRequest[requestFieldMaxTokens] = 1
-	completionRequest[requestFieldMaxCompletionTokens] = 1
-	completionRequest[requestFieldMaxOutputTokens] = 1
-	completionRequest[requestFieldMinTokens] = 1
+	for _, field := range tokenLimitFields {
+		completionRequest[field] = 1
+	}
 
 	pbody, err := json.Marshal(completionRequest)
 	if err != nil {
@@ -523,21 +534,11 @@ func (s *Server) runNIXLProtocolV2WriteParallel(
 	if streamOptionsOk {
 		completionRequest[requestFieldStreamOptions] = streamOptionsValue
 	}
-	delete(completionRequest, requestFieldMaxTokens)
-	if maxTokensOk {
-		completionRequest[requestFieldMaxTokens] = maxTokensValue
-	}
-	delete(completionRequest, requestFieldMaxCompletionTokens)
-	if maxCompletionTokensOk {
-		completionRequest[requestFieldMaxCompletionTokens] = maxCompletionTokensValue
-	}
-	delete(completionRequest, requestFieldMaxOutputTokens)
-	if maxOutputTokensOk {
-		completionRequest[requestFieldMaxOutputTokens] = maxOutputTokensValue
-	}
-	delete(completionRequest, requestFieldMinTokens)
-	if minTokensOk {
-		completionRequest[requestFieldMinTokens] = minTokensValue
+	for _, sv := range savedTokenValues {
+		delete(completionRequest, sv.field)
+		if sv.present {
+			completionRequest[sv.field] = sv.val
+		}
 	}
 
 	// Synthesise decode-leg kv_transfer_params that the serial path would

Gregory-Pereira/llm-d-inference-scheduler

pkg/sidecar/proxy/connector_ec_nixl.go
@@ -83,7 +83,7 @@ func (s *Server) fanoutEncoderCollect(
 // handleECNIXL fans out per-image encoder requests, aggregates each
 // response's ec_transfer_params into the prefill request body, and hands
 // off to the configured P/D connector.
-func (s *Server) handleECNIXL(w http.ResponseWriter, r *http.Request, prefillEndPoint string, encodeEndPoints []string) {
+func (s *Server) handleECNIXL(w http.ResponseWriter, r *http.Request, prefillEndPoint string, encodeEndPoints []string, apiType APIType) {
 	s.logger.V(logging.DEBUG).Info("running EC-NIXL protocol", "prefiller", prefillEndPoint, "encoderCount", len(encodeEndPoints))
 
 	_, body, ok := s.readJSONBody(r, w)
@@ -126,5 +126,5 @@ func (s *Server) handleECNIXL(w http.ResponseWriter, r *http.Request, prefillEnd
 		}
 	}
 
-	s.runPDPipeline(w, r, body, prefillEndPoint, requestID)
+	s.runPDPipeline(w, r, body, prefillEndPoint, requestID, apiType)
 }

pkg/sidecar/proxy/connector_ec_shared_storage.go
@@ -38,7 +38,7 @@ func (s *Server) fanoutEncoderPrimer(ctx context.Context, originalRequest map[st
 }
 
 // handleECSharedStorage handles an Encoder-Prefiller-Decoder disaggregation request
-func (s *Server) handleECSharedStorage(w http.ResponseWriter, r *http.Request, prefillEndPoint string, encodeEndPoints []string) {
+func (s *Server) handleECSharedStorage(w http.ResponseWriter, r *http.Request, prefillEndPoint string, encodeEndPoints []string, apiType APIType) {
 	s.logger.V(logging.DEBUG).Info("running EPD protocol", "prefiller", prefillEndPoint, "encoderCount", len(encodeEndPoints))
 
 	_, body, ok := s.readJSONBody(r, w)
@@ -67,5 +67,5 @@ func (s *Server) handleECSharedStorage(w http.ResponseWriter, r *http.Request, p
 		}
 	}
 
-	s.runPDPipeline(w, r, body, prefillEndPoint, requestID)
+	s.runPDPipeline(w, r, body, prefillEndPoint, requestID, apiType)
 }

pkg/sidecar/proxy/connector_nixlv2.go
@@ -76,7 +76,7 @@ func (s *Server) handleNIXLV2(w http.ResponseWriter, r *http.Request, prefillPod
 	if s.config.MoRIIOParallelDispatch && s.config.MoRIIOWriteMode {
 		// MoRI-IO requires transfer_id to carry the "tx" prefix for message routing.
 		transferID := "tx" + uuidStr
-		s.runNIXLProtocolV2WriteParallel(w, r, original, body, uuidStr, transferID, prefillPodHostPort, kvCacheSource)
+		s.runNIXLProtocolV2WriteParallel(w, r, original, body, uuidStr, transferID, prefillPodHostPort, kvCacheSource, apiType)
 		return
 	}
 
@@ -478,6 +478,7 @@ retryLoop:
 func (s *Server) runNIXLProtocolV2WriteParallel(
 	w http.ResponseWriter, r *http.Request, original []byte,
 	body map[string]any, uuidStr, transferID, prefillPodHostPort, kvCacheSource string,
+	apiType APIType,
 ) {
 	s.logger.V(logging.DEBUG).Info("running NIXL protocol V2 (concurrent dispatch)",
 		"url", prefillPodHostPort, "request_id", uuidStr)
@@ -490,10 +491,21 @@ func (s *Server) runNIXLProtocolV2WriteParallel(
 	// body; they are restored when building the decode body.
 	streamValue, streamOk := body[requestFieldStream]
 	streamOptionsValue, streamOptionsOk := body[requestFieldStreamOptions]
-	maxTokensValue, maxTokensOk := body[requestFieldMaxTokens]
-	maxCompletionTokensValue, maxCompletionTokensOk := body[requestFieldMaxCompletionTokens]
-	maxOutputTokensValue, maxOutputTokensOk := body[requestFieldMaxOutputTokens]
-	minTokensValue, minTokensOk := body[requestFieldMinTokens]
+
+	type savedField struct {
+		field   string
+		val     any
+		present bool
+	}
+	tokenLimitFields := tokenLimitFieldsForAPIType(apiType)
+	savedTokenValues := make([]savedField, len(tokenLimitFields))
+	for i, field := range tokenLimitFields {
+		if v, ok := body[field]; ok {
+			savedTokenValues[i] = savedField{field: field, val: v, present: true}
+		} else {
+			savedTokenValues[i] = savedField{field: field}
+		}
+	}
 
 	// Pin both legs to the same DP rank (kv_transfer_params + HTTP header).
 	dpRank := pickDPRank(uuidStr, s.config.MoRIIODPSize)
@@ -536,10 +548,9 @@ func (s *Server) runNIXLProtocolV2WriteParallel(
 
 	body[requestFieldStream] = false
 	delete(body, requestFieldStreamOptions)
-	body[requestFieldMaxTokens] = 1
-	body[requestFieldMaxCompletionTokens] = 1
-	body[requestFieldMaxOutputTokens] = 1
-	body[requestFieldMinTokens] = 1
+	for _, field := range tokenLimitFields {
+		body[field] = 1
+	}
 
 	pbody, err := json.Marshal(body)
 	if err != nil {
@@ -558,21 +569,11 @@ func (s *Server) runNIXLProtocolV2WriteParallel(
 	if streamOptionsOk {
 		body[requestFieldStreamOptions] = streamOptionsValue
 	}
-	delete(body, requestFieldMaxTokens)
-	if maxTokensOk {
-		body[requestFieldMaxTokens] = maxTokensValue
-	}
-	delete(body, requestFieldMaxCompletionTokens)
-	if maxCompletionTokensOk {
-		body[requestFieldMaxCompletionTokens] = maxCompletionTokensValue
-	}
-	delete(body, requestFieldMaxOutputTokens)
-	if maxOutputTokensOk {
-		body[requestFieldMaxOutputTokens] = maxOutputTokensValue
-	}
-	delete(body, requestFieldMinTokens)
-	if minTokensOk {
-		body[requestFieldMinTokens] = minTokensValue
+	for _, sv := range savedTokenValues {
+		delete(body, sv.field)
+		if sv.present {
+			body[sv.field] = sv.val
+		}
 	}
 
 	// Synthesise decode-leg kv_transfer_params that the serial path would